Fix kl divergence bug

meta_agents/algos/trpo.py
@@ -12,7 +12,7 @@ from meta_agents.torch_utils import np_to_torch, detach_distribution
 from meta_agents.samplers.base import SampleProcessor
 
 
-def surrogate_loss(samples, policy):
+def surrogate_loss(samples, policy, old_dist=None):
     assert isinstance(samples, dict)
     assert 'observations' in samples.keys()
     assert 'actions' in samples.keys()
@@ -25,15 +25,15 @@ def surrogate_loss(samples, policy):
     dist = policy(observations)
     # currently lets just detach the logprob
     # as old pi
-    if 'old_dist_info' not in samples.keys():
+    if old_dist is None:
         old_dist = detach_distribution(dist)
-    else:
-        old_dist = dist.__class__()(**samples['old_dist_info'])
+    
+    kl = torch.mean(kl_divergence(dist, old_dist))
 
     log_likeli_ratio = dist.log_prob(actions) - old_dist.log_prob(actions)
     ratio = torch.exp(log_likeli_ratio)
-    surr_loss = - torch.mean(ratio * advantages, dim=0)
-    return surr_loss, old_dist
+    surr_loss = -torch.mean(ratio * advantages, dim=0)
+    return surr_loss, old_dist, kl
 
 
 def conjugate_gradient(f_Ax, b, cg_iters=10, residual_tol=1e-10):
@@ -105,10 +105,9 @@ class TRPO(BatchPolopt):
         raise NotImplementedError
 
     def _trpo_step(self, samples, loss_func, constraint, cg_damping=1e-2,
-        ls_backtrack_ratio=.5, cg_iters=10, max_ls_steps=10, max_kl=1e-3,):
-        # Here, we do have detached old_dist so we dont need to do this
-        # in the future.
-        old_loss, old_dist = surrogate_loss(samples, self.policy)
+        ls_backtrack_ratio=.5, cg_iters=10, max_ls_steps=10, max_kl=1e-2,):
+
+        old_loss, old_dist, kl_before = surrogate_loss(samples, self.policy)
         grads = torch.autograd.grad(old_loss, self.policy.parameters())
         grads = parameters_to_vector(grads)
 
@@ -116,21 +115,21 @@ class TRPO(BatchPolopt):
         step_direction = conjugate_gradient(hessian_vector_product, grads, cg_iters)
 
         # Compute the Lagrange multiplier
-        shs = 0.5 * torch.dot(step_direction, hessian_vector_product(step_direction))
-        lagrange_multiplier = torch.sqrt(shs / max_kl)
+        shs = 0.5 * step_direction.dot(hessian_vector_product(step_direction))
+        lagrange_multiplier = torch.sqrt(max_kl / shs)
 
-        step = step_direction / lagrange_multiplier
-
+        grad_step = step_direction * lagrange_multiplier
         old_params = parameters_to_vector(self.policy.parameters())
 
         # Start line search
         step_size = 1.
         backtrack_step = 0
         for _ in range(max_ls_steps):
-            vector_to_parameters(old_params - step_size * step,
+            vector_to_parameters(old_params - step_size * grad_step,
                                  self.policy.parameters())
-            loss, _ = surrogate_loss(samples, self.policy)
-            kl = self.kl_divergence(samples, old_dists=old_dist)
+
+            loss, _, kl = surrogate_loss(samples, self.policy, old_dist=old_dist)
+
             improve = loss - old_loss
             if (improve.item() < 0.0) and (kl.item() < max_kl):
                 break
@@ -140,6 +139,10 @@ class TRPO(BatchPolopt):
             vector_to_parameters(old_params, self.policy.parameters())
             logger.log('Failed to update parameters')
         tabular.record('backtrack-iters', backtrack_step)
+        tabular.record('loss-before', old_loss.item())
+        tabular.record('loss-after', loss.item())
+        tabular.record('kl-before', kl_before.item())
+        tabular.record('kl-after', kl.item())
 
     def hessian_vector_product(self, samples_data, damping=1e-2):
         """Hessian-vector product, based on the Perlmutter method."""
@@ -157,13 +160,14 @@ class TRPO(BatchPolopt):
 
         return _product
 
-    def kl_divergence(self, samples, old_dists=None):
-        loss, old_dist = surrogate_loss(samples, self.policy)
-        updated_params = self.adapt_policy(loss)
+    def kl_divergence(self, samples, old_dist=None):
+        loss, old_dist_, kl = surrogate_loss(samples, self.policy)
+        if old_dist is None:
+            old_dist = old_dist_
 
         inputs = samples['observations']
-        new_dist = self.policy(inputs, params=updated_params)
-        kl = torch.mean(kl_divergence(new_dist, old_dist), dim=0)
+        new_dist = self.policy(inputs)
+        kl = torch.mean(kl_divergence(new_dist, old_dist))
         return kl
 
     def adapt_policy(self, loss, step_size=1., create_graph=True):

meta_agents/modules/gaussian_mlp_module.py
@@ -1,5 +1,6 @@
 """GaussianMLPModule."""
 import abc
+from collections import OrderedDict
 
 import torch
 from torch import nn
@@ -132,7 +133,7 @@ class GaussianMLPBaseModule(nn.Module, abc.ABC):
 
     def forward(self, inputs, params=None):
         """Forward method."""
-        mean, log_std_uncentered = self._get_mean_and_log_std(inputs)
+        mean, log_std_uncentered = self._get_mean_and_log_std(inputs, params=params)
 
         if self._min_std_param or self._max_std_param:
             log_std_uncentered = log_std_uncentered.clamp(
@@ -145,8 +146,13 @@ class GaussianMLPBaseModule(nn.Module, abc.ABC):
             std = log_std_uncentered.exp().exp().add(1.).log()
 
         cov = (std**2).diag_embed()
-        dist = MultivariateNormal(mean, cov)
 
+        try:
+            dist = MultivariateNormal(mean, cov)
+        except Exception as e:
+            import ipdb
+            ipdb.set_trace()
+            print('debug')
         return dist
 
     def _to_scalar_if_not_none(self, tensor):
@@ -202,13 +208,17 @@ class GaussianMLPModule(GaussianMLPBaseModule):
             layer_normalization=self._layer_normalization)
 
     def _get_mean_and_log_std(self, inputs, params=None):
-        mean = self._mean_module(inputs, params)
-
         if params is not None:
-            log_std = params['log_std']
+            offset = len('_mean_module') + 1
+            mean_params = {
+                name[offset:]: param
+                for name, param in params.items() if '_mean_module' in name
+            }
         else:
-            log_std = self._init_std  # this naming is confusing..
-
+            mean_params = None
+            std_params = None
+        mean = self._mean_module(inputs, mean_params)
+        log_std = self._init_std  # this naming is confusing..
         broadcast_shape = list(inputs.shape[:-1]) + [self._action_dim]
         uncentered_log_std = torch.zeros(
             *broadcast_shape) + log_std

meta_agents/policies/gaussian_mlp_policy.py
@@ -46,7 +46,7 @@ class GaussianMLPPolicy(GaussianMLPModule, Policy):
         # numpy to torch
         observations = torch.Tensor(observations)
         with torch.no_grad():
-            dist = self.forward(observations)
+            dist = self.forward(observations, params=params)
             actions = dist.rsample().detach().numpy()
 
         infos = dict()
@@ -60,5 +60,5 @@ class GaussianMLPPolicy(GaussianMLPModule, Policy):
 
     def get_action(self, observation, params=None):
         with torch.no_grad():
-            x = self.forward(observation)
+            x = self.forward(observation, params=params)
             return x.numpy(), dict()